b6d58eeb0d074c7bbc22068c82d31dcbc6bcfe2a
gmluqa <gmluqa@protonmail.com>
2023-09-21 13:06:04 +0200

    implemented the add item button, now need to have it dynamically place in column

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,7 +1,19 @@
 import "./AddTasks.scss";
 
+const createNewTask = () => {
+  sessionStorage.setItem("taskCreated", "true");
+
+  setTimeout(() => {
+    sessionStorage.setItem("taskCreated", "false");
+  }, 1);
+};
+
 function AddTask() {
-  return <div className="addTaskButtonBorder addTaskButton">+</div>;
+  return (
+    <div className="addTaskButtonBorder addTaskButton" onClick={createNewTask}>
+      +
+    </div>
+  );
 }
 
 export default AddTask;
